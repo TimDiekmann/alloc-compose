96d5d093c489e796d7ec89594decbce09c47cbb0
TimDiekmann <21277928+TimDiekmann@users.noreply.github.com>
2020-04-24 12:27:25 +0000

    Deploying to gh-pages from  @ c29814ab3c22cb4db33903815c63bb03728e5765 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["alloc_compose"] = {"name":"","files":["fallback_alloc.rs","lib.rs","null_alloc.rs","segregate_alloc.rs"]};
+sourcesIndex["alloc_compose"] = {"name":"","files":["callback_ref.rs","fallback_alloc.rs","lib.rs","null_alloc.rs","proxy.rs","region.rs","segregate_alloc.rs","stats.rs"]};
 createSourceSidebar();
