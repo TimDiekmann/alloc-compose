e883c7f12dc757946803252c6065dee7fc4a1ec3
TimDiekmann <21277928+TimDiekmann@users.noreply.github.com>
2020-08-10 20:57:49 +0000

    Deploying to gh-pages from  @ fbcfb2149b039d4ab407a436ebd037a436dcb59e 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["alloc_compose"] = {"name":"","files":["affix.rs","callback_ref.rs","chunk.rs","fallback.rs","helper.rs","lib.rs","macros.rs","null.rs","proxy.rs","region.rs","stats.rs"]};
+sourcesIndex["alloc_compose"] = {"name":"","files":["affix.rs","callback_ref.rs","chunk.rs","fallback.rs","helper.rs","lib.rs","macros.rs","null.rs","proxy.rs","region.rs","segregate.rs","stats.rs"]};
 createSourceSidebar();
